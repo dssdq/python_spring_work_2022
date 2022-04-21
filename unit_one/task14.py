c7dd1e09d0c7e55871222c358d0bf00dca3474f1
dssdq <oagoth@yandex.ru>
2022-04-21 20:25:36 +0300

    upd 21/04/22

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -5,27 +5,32 @@ mass = [1,2,17,54,30,89,2,1,6,2]
 Для числа 1 индексы двух ближайших чисел: 0 и 7
 Для числа 2 индексы двух ближайших чисел: 6 и 9
 '''
-#mass = [1,2,17,54,30,89,2,1,6,2]
-#chislo = 2
-
-#mass = input('Задайте массив чисел через запятую: ').split(",")  # данный ввод не работает,
-# т.к. элементы списка должны быть int, я не понимаю как это сделать ,без map
-
-mass = list(map(int,input('Задайте массив чисел через запятую: ').split(",")))
-chislo = int(input('введите число для поиска индексов: '))
-
-indexes = []
-for i in range(0,len(mass)):                       #перебор по всему диапазону массива
-    if mass[i] == chislo:                                    #если число введённое пользователем есть в массиве
-        indexes.append(i)                                  #добавить индекс последним в список
-
-indexes.sort(reverse = True)                            #от большего к меньшему
-print("индексы двух ближайших от числа -",chislo, "из массива",mass,'=',indexes[0:2], sep=' ')
-
-
-'''
+mass = [1,2,17,54,30,89,2,1,6,2]
 print(mass)
-print(type(mass[1]),type(chislo))
-'''
+user_number = int(input('введите число для поиска индексов в массиве: '))
+s = 0
+for i in mass:
+    if i == user_number and mass.count(user_number) == 1:   #перебираем массив и если только одно число выводим
+        print('Индекс числа: ', mass.index(i))
+    if i == user_number and mass.count(user_number) == 2: #перебираем массив и если только 2 включения выводим последовательно
+        print(mass.index(i), mass.index(i, 1, ))
+        break
+    if i == user_number and mass.count(user_number) > 2:  # если более двух считаем стартовый индекс, увеличивая
+        s = mass.index(i) + 1
+        if mass.index(i,s+mass.index(i,s,)) > mass.index(i) < mass.index(i,s,):
+            print(mass.index(i, s,) , mass.index(i, s + mass.index(i, s,)) )
+            break
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ unit_one/task19.py
@@ -0,0 +1,14 @@
+#todo: Требуется создать csv-файл «algoritm.csv» со следующими столбцами:
+'''– id - номер по порядку (от 1 до 10);
+– текст из списка algoritm"
+'''
+#Каждое значение из списка должно находится на отдельной строке.
+algoritm = [ "C4.5" , "k - means" , "Метод опорных векторов" , "Apriori" ,
+"EM" , "PageRank" , "AdaBoost", "kNN" , "Наивный байесовский классификатор" , "CART" ]
+
+f = open("algoritm.csv", mode="wt", encoding="utf-8")
+id = list(range(1,11))
+
+for index in range(len(id)):
+    f.writelines(str(id[index]) + ';' + str(algoritm[index] + "\r"))
+
